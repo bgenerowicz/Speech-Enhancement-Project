9d567b68e3d8589a633fffe9959e4fd526aa84a9
Bgenerowicz <basgen@wlan-145-94-232-66.wlan.tudelft.nl>
2017-05-24 16:53:52 +0200

    removed spam

--- Basmain.py
+++ Basmain.py
@@ -35,6 +35,9 @@ data_han_seg_over = np.multiply(hanning_segment,data_seg_over)
 data_han_seg_over = data_seg_over
 F_data = np.fft.fft(data_han_seg_over)
 
+#do stuff
+
+
 #IFFT
 IF_data = np.fft.ifft(F_data)
 IF_data_array = np.ravel(IF_data)
@@ -57,16 +60,16 @@ residual = data_extended - reconstructed_data
 
 
 #Plots
-# f, axarr = plt.subplots(3, sharex=True)
-# axarr[2].plot(residual)
-# axarr[2].set_title('Residual')
-# pylab.ylim([-0.5, 0.5])
-# axarr[1].plot(reconstructed_data)
-# axarr[1].set_title('Reconstructed')
-# axarr[0].plot(data_extended)
-# axarr[0].set_title('Original')
-#
-# plt.show()
+f, axarr = plt.subplots(3, sharex=True)
+axarr[2].plot(residual)
+axarr[2].set_title('Residual')
+pylab.ylim([-0.5, 0.5])
+axarr[1].plot(reconstructed_data)
+axarr[1].set_title('Reconstructed')
+axarr[0].plot(data_extended)
+axarr[0].set_title('Original')
+
+plt.show()
 
 print("--- %s seconds ---" % (time.time() - start_time))
 end = 1
