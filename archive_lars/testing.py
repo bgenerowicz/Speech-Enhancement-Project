bf575d88ff7ea24df2759b57a8cb7d55a35d54bc
Lars Rehbein <rehbein.lars@googlemail.com>
2017-06-15 10:57:33 +0200

    nothing

--- archive_lars/testing.py
+++ archive_lars/testing.py
@@ -4,32 +4,32 @@ import numpy as np
 ### First half: demonstration of averaging with random values
 ##
 
-# windowlength=5
-# arrtosearch=np.random.rand(30,5)
-# numrows=arrtosearch.shape[0]
-#
-# for rowstart,rowend in zip(range(0,numrows-windowlength,windowlength),range(windowlength-1,numrows,windowlength)):
-#     print(rowstart)
-#     print(rowend)
-#     for k in range(0,arrtosearch.shape[1]):
-#         arrtosearch[list(range(rowstart, rowend+1)), k] = min(arrtosearch[list(range(rowstart, rowend+1)), k])
-#
-#
-#
-# end=1
-#
+windowlength=5
+arrtosearch=np.random.rand(30,5)
+numrows=arrtosearch.shape[0]
+
+for rowstart,rowend in zip(range(0,numrows-windowlength,windowlength),range(windowlength-1,numrows,windowlength)):
+    print(rowstart)
+    print(rowend)
+    #for k in range(0,arrtosearch.shape[1]):
+    arrtosearch[list(range(rowstart, rowend+1)), list(range(0, arrtosearch.shape[1]))] = min(arrtosearch[list(range(rowstart, rowend+1)), list(range(0, arrtosearch.shape[1]))])
+
+
+
+end=1
+
 
 
 ## 2nd half, Some not so important np.nan calculations
 #
-Qprev=np.random.rand(1,10)
-alpha=np.random.rand(1,10)
-alpha[0,list(range(0,10))]=np.nan
-psd_row=np.random.rand(1,10)
-result=Qprev/alpha
-onevector=np.array(np.ones(10))
-result2=onevector-alpha
-
-Q = alpha*Qprev + (onevector-alpha) * psd_row
+# Qprev=np.random.rand(1,10)
+# alpha=np.random.rand(1,10)
+# alpha[0,list(range(0,10))]=np.nan
+# psd_row=np.random.rand(1,10)
+# result=Qprev/alpha
+# onevector=np.array(np.ones(10))
+# result2=onevector-alpha
+#
+# Q = alpha*Qprev + (onevector-alpha) * psd_row
 
 end=1
