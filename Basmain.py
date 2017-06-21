11160800f579780746a76507cf04652d9b47eadc
Bgenerowicz <basgen@wlan-145-94-234-17.wlan.tudelft.nl>
2017-06-21 16:13:53 +0200

    Power Spectral Subtraction basics implemented (not yet fully working)

--- Basmain.py
+++ Basmain.py
@@ -9,6 +9,7 @@ noise = 1  #Add noise? 1 = noise
 
 data, fs = b_f.import_data(filelocation,noise) #import data (possibly with noise)
 
+data = data/max(data)
 
 s_segment = int(tsegment * fs) # Calculate segment and overlap
 s_overlap = int(overlap * s_segment)
@@ -19,10 +20,20 @@ fft_data = b_f.transform_data(framed_data) #FFT of every segment
 ##########
 #Do stuff#
 ##########
-ifft_data = b_f.i_transform_data(fft_data) #inverse transform data
+y_k = b_f.bas_bartlett(framed_data)
+
+s_PSD_est = b_f.signal_PSD_estimate(y_k)
+
+
+s_est = b_f.signal_estimate(s_PSD_est,fft_data)
+
+
+ifft_data = b_f.i_transform_data(s_est) #inverse transform data
 reconstructed_data = b_f.overlap_add(ifft_data,len(data),s_segment, s_overlap) # Overlap & add
 b_f.make_plot(data,reconstructed_data) # Make plots
 
+# b_f.play_array(data,fs)
+# b_f.play_array(reconstructed_data,fs)
 
 end =1
 
--- bas_functions.py
+++ bas_functions.py
@@ -2,12 +2,16 @@ import numpy as np
 import soundfile as sf
 import matplotlib.pyplot as plt
 import pylab
+import cmath as cm
+import sounddevice as sd
 
 def frame_data(data,fs,s_segment,s_overlap):
 
     # pad data with zeros
     remainder = s_segment - (len(data) % s_segment)
     data_extended = np.ravel(np.asmatrix(np.pad(data, (0, int(remainder)), 'constant')))
+
+
 
     # Create & Apply Hanning to segment & overlap data
     num_frames = int((len(data_extended) - s_overlap) / s_overlap)
@@ -23,8 +27,8 @@ def frame_data(data,fs,s_segment,s_overlap):
 
 def i_transform_data(F_data):
     # IFFT
-    IF_data = np.fft.ifft(F_data)
-
+    data = np.fft.ifft(F_data)
+    IF_data = np.real(data)
     return IF_data
 
 
@@ -47,6 +51,8 @@ def import_data(filelocation,noise):
 
 def make_plot(data,reconstructed_data):
     # Calculate Residual
+    # data = data / max(data)
+    # reconstructed_data = reconstructed_data / max(reconstructed_data)
     residual = data - reconstructed_data
 
     # Plots
@@ -74,9 +80,57 @@ def overlap_add(IF_data,len_data,s_segment,s_overlap):
         reconstructed_data[s_start:(s_start + s_segment)] = reconstructed_data[s_start:(s_start + s_segment)] + IF_data[
                                                                                                                 i, :]
         s_start = s_start + s_overlap
-
+    reconstructed_data = reconstructed_data / max(reconstructed_data) #normalize
     return reconstructed_data
 
 def transform_data(framed_data):
     F_data = np.fft.fft(framed_data)
+
     return F_data
+
+def bas_bartlett(framed_data):
+
+
+
+    # Bartlett
+    bartlett_samples = 20
+    bartlett_frames = int(framed_data.shape[1] / bartlett_samples)
+    F_data = np.zeros(framed_data.shape)
+
+    # for i in range(0, bartlett_frames):
+    #     F_data[1, :] = F_data[1, :] + np.absolute(np.fft.fft(framed_data[1000, bartlett_samples * i:bartlett_samples * (i + 1)], framed_data.shape[1]))
+
+    for j in range(0, framed_data.shape[0]):
+        for i in range(0, bartlett_frames):
+            F_data[j, :] = F_data[j, :] + np.absolute(np.fft.fft(framed_data[j, bartlett_samples * i:bartlett_samples * (i + 1)], framed_data.shape[1])) ** 2
+        F_data[j, :] = F_data[j, :] / framed_data.shape[1]
+
+
+    return F_data
+        # F_data = np.zeros(framed_data.shape)   #TODO: Fix this at some point
+
+def signal_PSD_estimate(y_k):
+    s_PSD_est = np.zeros(y_k.shape)
+    for i in range(0,y_k.shape[0]):
+        s_PSD_est[i,:] = np.maximum(y_k[i,:],0.2*max(y_k[i,:]))
+        # print(min(s_PSD_est[i,:]))
+    # s_PSD_est = y_k
+    return s_PSD_est
+    # plt.plot(y_k[1000,:])
+    # plt.show()
+
+def signal_estimate(s_PSD_est,fft_data):
+    s_est = np.array(np.zeros(fft_data.shape),dtype='complex_')
+
+    angles = np.angle(fft_data)
+
+    for i in range(0,fft_data.shape[0]):
+        phase = np.exp(np.multiply(1j,angles[i,:]))
+        magnitude = np.sqrt(s_PSD_est[i,:])
+        s_est[i,:] = np.multiply(magnitude,phase)
+
+    return s_est
+
+def play_array(data,fs):
+    data = data / max(data)
+    sd.play(data,fs)
