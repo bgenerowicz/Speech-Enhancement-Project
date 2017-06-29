8ff05b39ee3e3a10ec41a5872862991329fa2bbc
Lars Rehbein <rehbein.lars@googlemail.com>
2017-06-29 18:59:06 +0200

    sorted everything out so it looks nice

--- Basmain.py
+++ Basmain.py
@@ -1,21 +1,76 @@
 import bas_functions as b_f
 import larsfunctions as l_f
+import Noise_MMSE as c_f
+
+
+
 
 # Variables
 tsegment = 20e-3  # 20ms segment
 overlap = 0.5
-filelocation = 'Audio/clean.wav'
-noise = 'Audio/n.wav'
+filelocation_clean = 'Audio/clean.wav'
+filelocation_noise = 'Audio/n.wav'
 
 
-data, fs = b_f.import_data(filelocation,noise) #import data (possibly with noise)
+s_n, fs = b_f.import_data(filelocation_clean) #import signal
+n_n, fs = b_f.import_data(filelocation_noise) #import noise
 
+n_n = 30*n_n  #Scale the noise
+y_n = s_n + n_n #Add scaled noise to signal
 s_segment = int(tsegment * fs) # Calculate segment and overlap
 s_overlap = int(overlap * s_segment)
+windowlength = int(1.5 / tsegment)
 
 
-framed_data = b_f.frame_data(data,fs,s_segment,s_overlap) #Frame data using hanning
-y_k = b_f.transform_data(framed_data) #FFT of every segment, # Step 1 of slide 76, lecture 1
+
+
+# b_f.make_plot(y_n,n_n) # Make plots
+s_n_framed = b_f.frame_data(s_n,fs,s_segment,s_overlap)
+n_n_framed = b_f.frame_data(n_n,fs,s_segment,s_overlap) #Frame data using hanning
+y_n_framed = b_f.frame_data(y_n,fs,s_segment,s_overlap)
+
+
+
+
+y_k = b_f.transform_data(y_n_framed) #FFT of every segment
+
+Py_true = b_f.temp_psd(y_n_framed)
+Pn_true = b_f.temp_psd(n_n_framed) #Pn using exact noise
+
+
+Pn_est = c_f.Noise_MMSE(y_n_framed,s_segment,y_k)
+# Pn_est = l_f.calculate_noisepsd_min(Py_true,tsegment,windowlength)
+
+
+# Py_est = b_f.bas_bartlett(y_n_framed) # Use bartlett to estimate Py
+
+
+
+# b_f.make_recon_plot(Py_true[:,100],Py_est[:,100]) # Make plots
+
+
+Sk_est = b_f.wiener(Py_true,Pn_est,y_k)
+
+ifft_data = b_f.i_transform_data(Sk_est) #inverse transform data
+reconstructed_data = b_f.overlap_add(ifft_data,len(y_n),s_segment, s_overlap) # Overlap & add
+
+# snr = b_f.bas_SNR(y_n_framed,ifft_data, s_n, reconstructed_data,y_n,fs) #wrong
+#SNR?
+dSNR1 = b_f.bas_SNR(s_n_framed,y_n_framed)   #SNR of noisy signal before recon
+dSNR2 = b_f.bas_SNR(s_n_framed,ifft_data)
+b_f.plot_SNR(dSNR1,dSNR2) # Make plots
+
+
+b_f.make_recon_plot(y_n,reconstructed_data) # Make plots
+
+
+end = 1
+# s_segment = int(tsegment * fs) # Calculate segment and overlap
+# s_overlap = int(overlap * s_segment)
+#
+# framed_noise = b_f.frame_data(noise_only,fs,s_segment,s_overlap)
+# framed_data = b_f.frame_data(data,fs,s_segment,s_overlap) #Frame data using hanning
+# y_k = b_f.transform_data(framed_data) #FFT of every segment, # Step 1 of slide 76, lecture 1
 
 # Appling slide 76, lecture 1
 # Py = b_f.bas_bartlett(framed_data) # Step 2 of slide 76, lecture 1
@@ -24,21 +79,29 @@ y_k = b_f.transform_data(framed_data) #FFT of every segment, # Step 1 of slide 7
 # ifft_data = b_f.i_transform_data(Sk_est) #inverse transform data, # Step 5 of slide 76, lecture 1
 
 # Applying slide 17, lecture 4
-Py = b_f.bas_bartlett(framed_data) #
+# Py = b_f.bas_bartlett(framed_data) #
+# Py_not ,Pn = b_f.temp_psd(framed_data,framed_noise)
 
-windowlength = int(1.5 / tsegment)
-Pn = l_f.calculate_noisepsd_min(Py,tsegment,windowlength)
 
-Sk_est = b_f.wiener(Py,Pn,y_k)
-ifft_data = b_f.i_transform_data(Sk_est) #inverse transform data
 
-reconstructed_data = b_f.overlap_add(ifft_data,len(data),s_segment, s_overlap) # Overlap & add
-b_f.make_plot(data,reconstructed_data) # Make plots
+# windowlength = int(1.5 / tsegment)
+# Pn_est = l_f.calculate_noisepsd_min(Py,tsegment,windowlength)
+# Pnest = b_f.Bas_min_ptrack(Py,fs)
+
+# Sk_est = b_f.wiener(Py,Pn,y_k)
+# ifft_data = b_f.i_transform_data(Sk_est) #inverse transform data
+#
+# reconstructed_data = b_f.overlap_add(ifft_data,len(data),s_segment, s_overlap) # Overlap & add
+# b_f.make_plot(data,reconstructed_data) # Make plots
+
+#Testing
+
+# b_f.plot_minPtrack(framed_noise,Pn_est,fs)
 
 # b_f.play_array(data,fs)
 # b_f.play_array(reconstructed_data,fs)
 
-end =1
+# end =1
 
 
 
--- larsmain.py
+++ larsmain.py
@@ -5,122 +5,129 @@ import numpy as np
 import pylab
 import sounddevice as sd
 import matplotlib
-#matplotlib.use("Agg")
 import matplotlib.pyplot as plt
 import wave
 import time
+import copy
 
-from larsfunctions import import_data
-from larsfunctions import import_frame_data
-from larsfunctions import transform_data
-#from larsfunctions import i_transform_data
-#from larsfunctions import overlap_add
-from larsfunctions import calculate_residual
-from larsfunctions import calculate_noisepsd_min
-from larsfunctions import calculate_speechpsd_heuristic
-from larsfunctions import calculate_wiener_gain
-from larsfunctions import calculate_noisepsd_min_costas
-from larsfunctions import wiener
-from larsfunctions import Noise_MMSE
-from larsfunctions import bartlett
-from larsfunctions import exponentialsmoother
-from larsfunctions import ml_estimation
-from larsfunctions import dd_approach
 
-from bas_functions import bas_bartlett
-from bas_functions import i_transform_data
-from bas_functions import overlap_add
-#from bas_functions import import_data
+from bas_functions import import_data
 from bas_functions import frame_data
 from bas_functions import transform_data
+from larsfunctions import bartlett
+from larsfunctions import calculate_noisepsd_min
+from larsfunctions import Noise_MMSE
+from larsfunctions import wiener
+from bas_functions import i_transform_data
+from larsfunctions import ml_estimation
+from larsfunctions import dd_approach
+from bas_functions import overlap_add
 from bas_functions import play_array
+from bas_functions import bas_SNR
+
+
 
 tsegment = 20e-3
-filelocation = 'Audio/clean+10n.wav'
+filelocation = 'Audio/clean+n.wav'
 windowlength = int(1.5 / tsegment)  # segment in seconds to find minimum psd, respectively psd of noise
 overlap = 0.5
-newdata,fs= import_data(filelocation)
+#newdata,fs= import_data(filelocation)
 filelocation_clean = 'Audio/clean.wav'
 newdata_clean,_= import_data(filelocation_clean)
-noise_location = 'Audio/10n.wav'
+noise_location = 'Audio/n.wav'
 alpha_smooth_exponential=0.85
 alpha_smooth_dd=0.96
 eps=0.0008
-rmsarray_han,fs,remainder = import_frame_data(filelocation,tsegment)
+
+
+## Import Data
+signal, fs = import_data(filelocation) #import signal
+signal_clean, _ = import_data(filelocation_clean) #import clean signal
+signal_noise,_ = import_data(noise_location) #import noise
+
 
 s_segment = int(tsegment * fs) # Calculate segment and overlap
 s_overlap = int(overlap * s_segment)
 
-F_data=transform_data(rmsarray_han)
+
+## Applying Framing and Hanning
+signal_han = frame_data(signal,fs,s_segment,s_overlap)
+signal_clean_han = frame_data(signal_clean,fs,s_segment,s_overlap)
+signal_noise_han = frame_data(signal_noise,fs,s_segment,s_overlap)
+
+
+## FT and Signal Power
+F_data=transform_data(signal_han)
 psd_F_data=np.absolute(F_data)**2
 
-## Noise import
-rmsarray_han_noise,fs,_ = import_frame_data(noise_location,tsegment)
-F_data_noise = transform_data(rmsarray_han_noise)
+F_data_noise = transform_data(signal_noise_han)
 psd_F_data_noise = np.absolute(F_data_noise)**2
 
 
+## Smoothing PSD
 F_data_bartlett = bartlett(psd_F_data) #bartlett smoothing
-F_data_exponential = exponentialsmoother(psd_F_data,alpha_smooth_exponential) #exponential smoothing
+#F_data_exponential = exponentialsmoother(psd_F_data,alpha_smooth_exponential) #exponential smoothing
+F_data_smoothed = copy.copy(F_data_bartlett)
 
-noisevariance_min,alphastack = calculate_noisepsd_min(F_data_exponential,tsegment,windowlength) #calculate Pn with minimum tracking
-noisevariance_mmse = Noise_MMSE(rmsarray_han,F_data_exponential,s_segment)
 
-noisevariance = noisevariance_mmse
+## Calculate Noise PSD
+noisevariance_min,alphastack = calculate_noisepsd_min(F_data_smoothed,tsegment,windowlength) #calculate Pn with minimum tracking
+noisevariance_mmse = Noise_MMSE(signal_han,F_data,s_segment)
+noisevariance = noisevariance_min
 
+
+## Apply Wiener Gain
 s_est = wiener(F_data_bartlett,noisevariance,F_data)
 ifft_data = i_transform_data(s_est)
-reconstructed_data = overlap_add(ifft_data,len(newdata),s_segment,s_overlap)
+reconstructed_data = overlap_add(ifft_data,len(signal),s_segment,s_overlap)
 
 
-remainder=np.empty(10)
-residual = calculate_residual(filelocation,reconstructed_data,remainder)
-
-s_est_mmse=wiener(F_data,psd_F_data,F_data)
-
+## ML and DD Approach
+sigma_s_ml = ml_estimation(F_data_smoothed,noisevariance)
+sigma_s_dd = dd_approach(sigma_s_ml,noisevariance,F_data_smoothed,alpha_smooth_dd,eps)
 
 
-sigma_s_ml_7 =ml_estimation(F_data_exponential,noisevariance)
-
-
-sigma_s_dd = dd_approach(sigma_s_ml_7,noisevariance,F_data_exponential,alpha_smooth_dd,eps)
-
-
+## Calculate Wiener Gain
 noisevariance[noisevariance == 0] = np.nan
 gainmatrix = sigma_s_dd / (sigma_s_dd + noisevariance)
 gainmatrix[np.isnan(gainmatrix) ] = 1
-
-#gainmatrix = 1-noisevariance_mmse/F_data_exponential
 gainmatrix = np.maximum(gainmatrix,0)
 
+## Apply Gain
+s_est_min = F_data_smoothed * gainmatrix
 
 
-s_est_min = F_data_exponential * gainmatrix
+## IFFT & Reconstruct
 ifft_data = i_transform_data(s_est_min)
+reconstruction = overlap_add(ifft_data,len(signal),s_segment,s_overlap)
+
+## Calculate SNR
+snr_a=bas_SNR(signal_clean_han,signal_han)
+snr_a[np.isnan(snr_a)]=0
+
+snr_b=bas_SNR(signal_clean_han,ifft_data)
+snr_b[np.isnan(snr_b)]=0
 
 
 
-reconstruction = overlap_add(ifft_data,len(newdata),s_segment,s_overlap)
+## PLOTS
 
-
-#sf.write('new_file3_mmse.ogg', reconstructed_data, fs)
-
-y=10*np.log10(psd_F_data_noise[:,225])
+y=10*np.log10(psd_F_data_noise[:,180])
 y[y == 0] = np.nan
 x_axis2 = 320*np.array(range(0,y.size))/fs
 signalpowerplot=plt.plot(x_axis2,y,color = 'g',alpha=0.4, label="Noise Power")
 
-y=10*np.log10(F_data_exponential[:,225])
+y=10*np.log10(F_data_smoothed[:,180])
 y[y == 0] = np.nan
 x_axis2 = 320*np.array(range(0,y.size))/fs
 exponential_plot=plt.plot(x_axis2,y,color = 'g',alpha=0.9,  label="PSD Exponential Smoothed")
 
-y=10*np.log10(noisevariance_min[:,225])
+y=10*np.log10(noisevariance_min[:,180])
 y[y == 0] = np.nan
 x_axis2 = 320*np.array(range(0,y.size))/fs
 noisevarianceplot=plt.plot(x_axis2,y,color = 'r',alpha=0.8, label="Noise Variance (Minimum)")
 
-y=10*np.log10(noisevariance_mmse[:,225])
+y=10*np.log10(noisevariance_mmse[:,180])
 y[y == 0] = np.nan
 x_axis2 = 320*np.array(range(0,y.size))/fs
 noisevariance_mmse=plt.plot(x_axis2,y,color = 'r',alpha=0.5,  label="Noise Variance (MMSE)")
@@ -130,14 +137,14 @@ noisevariance_mmse=plt.plot(x_axis2,y,color = 'r',alpha=0.5,  label="Noise Varia
 plt.legend(bbox_to_anchor=(0., 1.02, 1., .102), loc=3, ncol=2, mode="expand", borderaxespad=0.)
 
 
-
+#
 # ## Plots for Exponential Smoothing
 # y=10*np.log10(psd_F_data[:,120])
 # y[y == 0] = np.nan
 # x_axis2 = 320*np.array(range(0,y.size))/fs
 # signalpowerplot=plt.plot(x_axis2,y,color = 'c',alpha=0.4, label="Noise Power")
 #
-# y=10*np.log10(F_data_exponential[:,120])
+# y=10*np.log10(F_data_smoothed[:,120])
 # y[y == 0] = np.nan
 # x_axis2 = 320*np.array(range(0,y.size))/fs
 # signalpowerplot=plt.plot(x_axis2,y,color = 'r',alpha=0.5, label="Noise Power")
